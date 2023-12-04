fe17e056aceda6983d117d7ac635ef88e00f592e
정지훈 <jungjihoons@naver.com>
2023-12-05 06:13:47 +0900

    #1 - abort 이벤트 발생 시 request도 취소하기
    thunk 실행이 중지가 되고 rejected action이 dispatch 되었지만 http request 계속 실행 -> 요청도 중단 -> AbortController : 인터페이스의 signal 읽기 전용 프로퍼티는 DOM 요청과 통신하거나 취소하는데 사용하는 AbortSignal 객체 인터페이스를 반환
    `https://developer.mozilla.org/ko/docs/Web/API/AbortController/signal`

--- src/features/counter/Test.tsx
+++ src/features/counter/Test.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from "react";
 import { useAppDispatch } from "../../app/hooks";
-import { incrementAsync } from "./counterSlice";
+import { fetchUsersAsync, incrementAsync } from "./counterSlice";
 
 const Test = () => {
   const dispatch = useAppDispatch();
   useEffect(() => {
-    const promise = dispatch(incrementAsync(10));
+    const promise = dispatch(fetchUsersAsync());
+
+    //const promise = dispatch(incrementAsync(10));
 
     return () => {
         promise.abort();
